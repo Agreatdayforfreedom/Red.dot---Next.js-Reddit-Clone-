32134d870eb0375848ac46d4c3de3767551c2c8d
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-03-22 10:57:30 -0300

    as child

--- components/auth/auth-actions.tsx
+++ components/auth/auth-actions.tsx
@@ -1,15 +1,14 @@
 import Link from "next/link";
-import { redirect } from "next/navigation";
 import React from "react";
 import { Button } from "@/components/ui/button";
 
 export default async function AuthActions() {
   return (
     <div>
-      <Button variant={"ghost"} className="m-1">
+      <Button variant={"ghost"} className="m-1" asChild>
         <Link href="/register">Sign up</Link>
       </Button>
-      <Button variant={"default"} className="m-1">
+      <Button variant={"default"} className="m-1" asChild>
         <Link href="/login">Log in</Link>
       </Button>
     </div>
