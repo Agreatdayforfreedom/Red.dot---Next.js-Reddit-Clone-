a196956952d21628f771e2aab3ebf64f6addb56b
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-03-22 16:37:21 -0300

    search

--- components/header.tsx
+++ components/header.tsx
@@ -4,6 +4,8 @@ import AuthActions from "@/components/auth/auth-actions";
 import PrivateHeader from "@/components/private-header";
 import Logo from "@/components/logo";
 
+import { Search } from "./search";
+
 export default async function Header() {
   const session = await auth();
 
@@ -11,6 +13,7 @@ export default async function Header() {
     <header className="fixed top-0 left-0 block w-full h-12 z-50 bg-white border-b  border-slate-600">
       <nav className="flex items-center mx-4 justify-between h-full">
         <Logo />
+        <Search />
 
         {session?.user ? <PrivateHeader /> : <AuthActions />}
       </nav>
---
+++ components/search.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { Community } from "@prisma/client";
+import Image from "next/image";
+import React, { useState } from "react";
+import { useCommunity } from "../store/use-community";
+import { Input } from "./ui/input";
+import { MdClose } from "react-icons/md";
+import { getContrastYIQ } from "../lib/yiq";
+
+export const Search = () => {
+  const [avoidCommunityFilter, setAvoidCommunityFilter] =
+    useState<boolean>(false);
+  const community = useCommunity((state) => state.community);
+
+  const handleAvoidCF = () => {
+    setAvoidCommunityFilter(true);
+  };
+
+  return (
+    <div className="flex rounded-lg p-1 border">
+      {community?.avatar && !avoidCommunityFilter && (
+        <div
+          className="space-x-1 p-1 flex items-center rounded-full"
+          style={{ background: `${community.background_color}60` }}
+        >
+          <div className="relative w-4 h-4">
+            <Image
+              fill
+              src={community.avatar}
+              alt="community image"
+              className="object-cover rounded-full"
+            />
+          </div>
+          <p className="text-sm font-semibold max-w-32 overflow-hidden text-ellipsis">
+            {community?.name}
+          </p>
+          <button onClick={handleAvoidCF}>
+            <MdClose
+              size={14}
+              className="rounded-full"
+              style={{
+                background: community.interactive_elements_color,
+                fill: getContrastYIQ(community.interactive_elements_color),
+              }}
+            />
+          </button>
+        </div>
+      )}
+      <Input
+        className="focus-visible:ring-0 border-none h-auto "
+        placeholder="Search..."
+      />
+    </div>
+  );
+};
