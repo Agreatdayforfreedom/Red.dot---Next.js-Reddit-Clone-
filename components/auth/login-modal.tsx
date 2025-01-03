38ee0ab7cd03e4601cf2adb7654b6d3c4218ad07
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2025-01-03 11:00:36 -0300

    fix propagation when the votes action opens login modal

--- components/auth/login-modal.tsx
+++ components/auth/login-modal.tsx
@@ -12,7 +12,10 @@ export default function LoginModal({
 }) {
   if (!open) return undefined;
   return (
-    <div className="fixed flex items-center justify-center w-full h-full top-0 left-0 z-50 bg-stone-900/50">
+    <div
+      onClick={(e) => e.stopPropagation()}
+      className=" cursor-default fixed flex items-center justify-center w-full h-full top-0 left-0 z-50 bg-stone-900/50"
+    >
       <div className="w-fit relative">
         <button className="absolute top-2 right-2 group" onClick={onClose}>
           <MdClose size={20} className="group-hover:hover:fill-red-500" />
